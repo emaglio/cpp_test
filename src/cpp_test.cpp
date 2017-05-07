8c0e202ca1d759ea24756fbe9d11db075aaa5103
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2017-05-07 15:30:07 +1000

    Fixed some stuff for First_Arduino

--- src/cpp_test.cpp
+++ src/cpp_test.cpp
@@ -11,20 +11,42 @@ using namespace std;
 #include<string.h>
 
 
-#define num_array 5
-#define dim_array 6
+#define num_array 4
+#define dim_array 5
 
 
 char index[] = {"1-/-/-/-/"};
 int index_dim = 9;
 
-void addArray(int row, const char* array[], int dim){
+//Menu
+char* level_1[3] = {"1","Sensors", "RBG LED"};
+char* level_1_1[3] {"1-1","Temperature", "Light"};
+char* level_1_1_1[3] {"1-1-1","ON", "OFF"};
+char* level_1_1_2[3] {"1-1-2","ON", "OFF"};
+char* level_1_2[5] {"1-2","Blue", "Green", "Red", "OFF"};
+long current_ok_counter = 0;
+long current_scroll_counter = 0;
+long ok_counter = 0;
+long scroll_counter = 0;
+int ok_result = 0;
+int scroll_result = 0;
+int size_array = 0;
+
+//used to get the correct array
+int remainder = 0;
+int menu_array_row = 0;
+
+
+//menu_array to identify the correct array to show
+char* menu_array[num_array][dim_array];
+
+void addArray(int row, char* array[], int dim){
 
 	for(int i=0; i<dim; i++){
 		menu_array[row][i] = array[i];
 	}
 
-	for(int i=dim-1; i<dim_array; i++){
+	for(int i=dim; i<dim_array; i++){
 		menu_array[row][i] = "nil";
 	}
 }
@@ -34,15 +56,19 @@ int getLength(char text[]){
 	int i=0;
 	while(i<dim){
 		if(text[i] == '/'){
+			if(i<dim){
+				i -=1 ;
+			}
+			break;
+		}
+		if((text[i] == NULL) || (text[i] == '\0')){
 			break;
 		}else{
 			i++;
 		}
 	}
-	if(i == dim){
+	if(i > dim){
 		i = dim;
-	}else{
-		i= i-1;
 	}
 	return i;
 }
@@ -62,12 +88,28 @@ bool checkChartMatch(char index[], int row){
 
 
 int getMenuRow(char index[]){
+	int length_index = getLength(index);
+	bool found =  false;
 	int value;
-	for(int i=0; i<num_array; i++){
-		if(checkChartMatch(index,i) == true){
-			value = i;
-			break;
+	for(int i=0; i<num_array && !found; i++){
+		// compare length of index first and then if they are equal
+		int length_menu_array_index = getLength(menu_array[i][0]);
+		if(length_index == length_menu_array_index){
+			char temp[length_index];
+			int num_char =0;
+			for(j=0; j<length_index; j++){
+				strcpy(temp, menu_array[i][0]);
+				if(temp[j] != index[j]){
+					break;
+				}else{
+					num_char += 1;
+				}
+			}
+			if(num_char == length_index){
+				found = true;
+			}
 		}
+		value = i;
 	}
 	return value;
 }
@@ -108,9 +150,9 @@ void backLevel(){
 }
 
 int getSizeArray(int row){
-	int i = 1;
+	int i = 0;
 	while(i < dim_array){
-		const char* temp = menu_array[row][i];
+		char* temp = menu_array[menu_array_row][i];
 		if(temp == "nil"){
 			break;
 		}else{
@@ -119,51 +161,40 @@ int getSizeArray(int row){
 	}
 
 	if(i < dim_array){
-		i++;
+		i -= 1;
 	}
 	return i;
 }
 
 int main() {
-	const char* level_1[6] = {"1","Sensors", "RBG LED", "Erin", "Mani", "Doggy dog"};
-	const char* level_1_1[4] {"1-1","Temperature", "Light", "<- BACK"};
-	const char* level_1_1_1[4] {"1-1-1-2-4","ON", "OFF", "<- BACK"};
-	const char* level_1_2[6] {"1-2","OFF", "Blue", "Green", "Red", "<- BACK"};
-	const char* level_1_3[2] {"1-3","<- BACK"};
-	const char* menu_array[num_array][dim_array];
-
-
-
-	addArray(0, level_1, 6);
+	addArray(0, level_1, 3);
 	addArray(1, level_1_1, 4);
 	addArray(2, level_1_1_1, 4);
-	addArray(3, level_1_2, 6);
-	addArray(5, level_1_3, 2);
+	addArray(3, level_1_1_2, 4);
+	addArray(4, level_1_2, 6);
 
-	char test2[] = {"1-1-1-2-4"};
-	int num = 2;
-	char num_char = '0' + num;
-	nextLevel(num_char);
-	nextLevel(num_char);
-	nextLevel(num_char);
-	nextLevel(num_char);
-	backLevel();
-	backLevel();
-	backLevel();
-	backLevel();
-	backLevel();
+	char test_index[] = {"1-1-/-/-/"};
+//	int num = 2;
+//	char num_char = '0' + num;
+//	nextLevel(num_char);
+//	nextLevel(num_char);
+//	nextLevel(num_char);
+//	nextLevel(num_char);
+//	backLevel();
+//	backLevel();
+//	backLevel();
+//	backLevel();
+//	backLevel();
 
-//	int test = getMenuRow(test2);
+	int row = getMenuRow(test_index);
 
-//	int test = getSizeArray(8);
+	int size = getSizeArray(row);
 
-	const char* array[dim_array];
-	for(int i = 1; i < dim_array; i++){
-		array[i-1] = menu_array[0][i];
+	const char* array[size];
+	for(int i = 0; i < (size); i++){
+		array[i] = menu_array[row][i+1];
 	}
 
-
-//	int test = 0;
-	cout << test2 << "\n" << array[0] << "\n" << array[0] << endl;
+	cout << row << "\n" << size << "\n" << array[0] << "\n" << array[1] << "\n" << array[2] << endl;
 	return 0;
 }
